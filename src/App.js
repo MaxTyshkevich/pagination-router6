4f0d311eb593e2a0c0dfc551128a5062dd6a8f90
Max Tyshkevich <2842778@gmail.com>
2022-07-13 20:27:26 +0300

    feat: add pagination and list link

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Container, Pagination, TextField, Stack, Link } from "@mui/material";
+
+const BASE_URL = `http://hn.algolia.com/api/v1/search?`;
 
 function App() {
+  const [posts, setPost] = useState([]);
+  const [query, setQuery] = useState("react");
+  const [page, setPage] = useState(1);
+  const [pageQty, setPageQty] = useState(0); // max count pages
+
+  console.log(`page: `, page);
+
+  useEffect(() => {
+    axios.get(BASE_URL + `query=${query}&page=${page - 1}`).then(({ data }) => {
+      console.log(data);
+      setPost(data.hits);
+      setPageQty(data.nbPages);
+    });
+  }, [query, page]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App" sx={{ mt: 3 }}>
+      <TextField
+        fullWidth
+        label="query"
+        value={query}
+        onChange={({ target }) => {
+          setQuery(target.value);
+          setPage(1);
+        }}
+      />
+
+      <Stack spacing={2} sx={{ mt: 2 }}>
+        {!!pageQty && (
+          <Pagination
+            count={pageQty}
+            page={page}
+            onChange={(_, newPage) => setPage(newPage)}
+            sx={{ mx: "auto" }}
+          />
+        )}
+
+        {posts.map((post) => (
+          <Link key={post.objectID} href={post.url}>
+            {post.title || post.story_title}
+          </Link>
+        ))}
+      </Stack>
+    </Container>
   );
 }
 
